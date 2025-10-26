a7f667bd5048152e26bb0cd2369c59cf4d035008
Caryl Anne Balbas <caryl.anne.balbas@eee.upd.edu.ph>
2025-10-26 09:46:06 +0000

    Fix SEO, fix login/register

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -91,18 +91,18 @@ const LoginModal = ({ showModal, closeModal, user, setUser, setSignupModalOpen }
         </Button>
       </Form>
       
-      <div className="text-center mt-3">
+      <div className="text-center mt-1">
         <span>or</span>
       </div>
 
-      <button className="mt-3 w-100 align-items-center d-flex justify-content-center btn border fw-bold text-white p-2" onClick={async () => {await handleGoogleLogin(); closeModal();}}>
+      <button className="mt-1 w-100 align-items-center d-flex justify-content-center btn border fw-bold text-white p-2" onClick={async () => {await handleGoogleLogin(); closeModal();}}>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-google" viewBox="0 0 16 16">
           <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"/>
         </svg> 
         <span className="ps-2"> Login with Google </span>
       </button>
     </Modal.Body>
-    <Modal.Footer className="bg-lightgreen border-top-0 justify-content-center">
+    <Modal.Footer className="bg-lightgreen border-top-0 justify-content-center pt-0 pb-4">
       <div className="text-center text-white">Looking to <span onClick={() => {closeModal(); setSignupModalOpen(true);}} className="text-orange fw-bold mouse-pointer">create an account</span> ?</div>
     </Modal.Footer>
   </Modal>
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -15,7 +15,7 @@ const SignupModal = ({ showModal, closeModal, user, setUser, setLoginModalOpen, 
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("")
   const [error, setError] = useState("")
-  const [phoneNumber, setPhoneNumber] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("+44");
   const [otp, setOtp] = useState("");
   const [confirmationResult, setConfirmationResult] = useState(null);
   const [step, setStep] = useState(`${phoneSetUp ? 'phoneEntry': 'default'}`); 
@@ -165,14 +165,13 @@ const SignupModal = ({ showModal, closeModal, user, setUser, setLoginModalOpen, 
   const resetValues = () => {
     console.log("hello")
     setError('')
-    setPhoneNumber('')
+    setPhoneNumber('+44')
     setPasswordError(false)
     setEmailError(false)
     setPhoneError('')
     setUsername("")
     setCheckingUsername(false);
     setUsernameAvailable(null)
-    setPhoneNumber("")
     setPassword("")
     setEmail("")
     
@@ -232,7 +231,7 @@ const SignupModal = ({ showModal, closeModal, user, setUser, setLoginModalOpen, 
       });
       const data = await response.json();
       setError('')
-      setPhoneNumber('')
+      setPhoneNumber('+44')
       setPasswordError(false)
       setEmailError(false)
       setPhoneError('')
@@ -401,7 +400,7 @@ const SignupModal = ({ showModal, closeModal, user, setUser, setLoginModalOpen, 
           </div>
         </Form.Group>
 
-        <Form.Group controlId="formPassword" className="mt-3">
+        <Form.Group controlId="formPassword">
           <Form.Label className="text-uppercase">Password</Form.Label>
           <Form.Control type="password" placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} required />
             <div className={`text-end fst-italic pt-1 font-12 ${!passwordError ? 'text-orange fw-bold' : ''}`}>
@@ -411,7 +410,7 @@ const SignupModal = ({ showModal, closeModal, user, setUser, setLoginModalOpen, 
                   : ''} &nbsp;
           </div>
         </Form.Group>
-        <Form.Group controlId="formPhone" className="mt-3">
+        <Form.Group controlId="formPhone">
         <div>
             <Form.Label>PHONE NUMBER</Form.Label>
             <Tooltip 
@@ -458,11 +457,11 @@ const SignupModal = ({ showModal, closeModal, user, setUser, setLoginModalOpen, 
           )}
         </Button>
       </Form>
-      <div className="text-center mt-3">
+      <div className="text-center mt-1">
         <span>or</span>
       </div>
 
-      <button className="mt-3 w-100 align-items-center d-flex justify-content-center btn border fw-bold text-white p-2" onClick={async () => {await handleGoogleSignup();  resetValues(); closeModal();}}>
+      <button className="mt-1 w-100 align-items-center d-flex justify-content-center btn border fw-bold text-white p-2" onClick={async () => {await handleGoogleSignup();  resetValues(); closeModal();}}>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-google" viewBox="0 0 16 16">
           <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"/>
         </svg> 
@@ -555,7 +554,7 @@ const SignupModal = ({ showModal, closeModal, user, setUser, setLoginModalOpen, 
       </Form>
     )}
     </Modal.Body>
-    <Modal.Footer className="bg-lightgreen border-top-0 justify-content-center">
+    <Modal.Footer className="bg-lightgreen border-top-0 justify-content-center mt-0 pt-0 pb-4">
     {step === "default" ? (
       <div className="text-center text-white">Already have an account? <span onClick={() => {closeModal(); setLoginModalOpen(true);}} className="text-orange fw-bold mouse-pointer">Login</span></div>
     ) : step === "phoneEntry" ? '' : ''}
